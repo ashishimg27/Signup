994d15342814d5612ae563f21a7cf1de116688a6
ashishimg27 <95354816+ashishimg27@users.noreply.github.com>
2021-12-08 18:00:44 +0530

    Add files via upload

---
+++ src/FormSuccess.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Form.css';
+
+const FormSuccess = () => {
+  return (
+    <div className='form-content-right'>
+      <h1 className='form-success'>Thankyou for beign part of MyPizaa family!</h1>
+      <img className='form-img-2' src='img/pizzat.png' alt='success-image' />
+    </div>
+  );
+};
+
+export default FormSuccess;
